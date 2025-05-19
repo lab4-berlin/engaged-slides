ac5cc8479a8f96d86e9aad9765004dee7df85366
Ilya <bkmy43@googlemail.com>
2025-05-19 13:13:01 +0200

    fixing deploymet to aws

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from app import create_app
 
-app = create_app() # Initialize the Flask app
+app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
